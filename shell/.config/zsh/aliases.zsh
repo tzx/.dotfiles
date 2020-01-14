962d924e66f225e817b1173947c5b97c276f8b97
Timmy Xiao <timmyxiao1@gmail.com>
2020-01-14 12:31:14 -0500

    Shell: more aliases, vim/git and bluelight

--- shell/.config/zsh/aliases.zsh
+++ shell/.config/zsh/aliases.zsh
@@ -1,7 +1,10 @@
 # ALIASES
 
 alias y="yay"
+alias v="nvim"
+alias g="git"
 alias ls="exa"
 alias cat="bat"
 alias xcp="xclip -selection clipboard"
 alias nobluelight="redshift -P -O 1900"
+alias bluelight="redshift -x"
