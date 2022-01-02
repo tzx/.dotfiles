6e92e5f5207c3554309f64ee4f1ec1dacc55e648
Timmy Xiao <34635512+tzx@users.noreply.github.com>
2022-01-02 17:35:37 -0500

    Add some plugins

--- .config/nvim/lua/packages.lua
+++ .config/nvim/lua/packages.lua
@@ -5,36 +5,56 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 return require('packer').startup(function()
-    use 'wbthomason/packer.nvim'
-    use 'sainnhe/gruvbox-material'
+  use 'wbthomason/packer.nvim'
+  use 'sainnhe/gruvbox-material'
 
-    use { 
-        'nvim-telescope/telescope.nvim', 
-        requires = { 'nvim-lua/plenary.nvim' },
-        config = [[ require('plugins.telescope') ]]
-    }
+  use { 
+    'nvim-telescope/telescope.nvim', 
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = [[ require('plugins.telescope') ]]
+  }
 
-    use 'nvim-treesitter/nvim-treesitter'
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+    requires = { 'nvim-treesitter/nvim-treesitter-textobjects' },
+    config = [[ require('plugins.treesitter') ]],
+  }
 
-    use {
-        'neovim/nvim-lspconfig', 
-        config = [[ require('plugins.lspconfig') ]]
-    }
+  use {
+    'neovim/nvim-lspconfig', 
+    config = [[ require('plugins.lspconfig') ]]
+  }
 
-    use { 
-      'hrsh7th/nvim-cmp',
-      requires = {
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-cmdline',
-      },
-      config = [[ require('plugins.cmp') ]]
-    }
+  use { 
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+    },
+    config = [[ require('plugins.cmp') ]]
+  }
 
-    if packer_bootstrap then
-        require('packer').sync()
-    end
+  use {
+    'folke/which-key.nvim',
+    config = function() require('which-key').setup() end, 
+  }
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    config = [[ require('plugins.lualine') ]],
+  }
+
+  use {
+    'lukas-reineke/indent-blankline.nvim',
+    config = [[ require('plugins.indent-blankline') ]]
+  }
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
