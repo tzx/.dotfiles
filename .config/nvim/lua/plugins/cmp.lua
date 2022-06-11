5550e33f0706ba0b4d6fab52ad5fab15d9aeb95f
Timmy Xiao <34635512+tzx@users.noreply.github.com>
2022-06-11 16:48:40 -0400

    LSP updates

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,9 +1,10 @@
-local cmp = require'cmp'
+local cmp = require("cmp")
+local luasnip = require("luasnip")
 
 cmp.setup({
   snippet = {
     expand = function(args)
-      require'luasnip'.lsp_expand(args.body)
+      luasnip.lsp_expand(args.body)
     end
   },
   mapping = cmp.mapping.preset.insert({
@@ -12,13 +13,32 @@ cmp.setup({
       c = cmp.mapping.close(),
     }),
     ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
     { name = 'path' },
-  }, {
-    { name = 'buffer' }, -- Only have buffer when the above has no results
+    { name = 'buffer' },
   })
 })
 
