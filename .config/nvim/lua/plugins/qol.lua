b98cf6dd58997455b98f9c38e7474e4f7695f349
Timmy Xiao <34635512+tzx@users.noreply.github.com>
2024-03-02 10:55:50 -0800

    Update + add gitlinker

--- .config/nvim/lua/plugins/qol.lua
+++ .config/nvim/lua/plugins/qol.lua
@@ -3,5 +3,11 @@ return {
     'numToStr/Comment.nvim',
     lazy = false,
     config = true,
-  }
+  },
+  {
+    'linrongbin16/gitlinker.nvim',
+    config = function()
+      require('gitlinker').setup()
+    end,
+  },
 }
